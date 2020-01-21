89bbbba8da8aa86961e7ee5b20c04dc6035d8b69
pksasso <patricksassorj@gmail.com>
2020-01-21 17:02:23 -0300

    * Mudanças no mapa

--- web/src/components/Map/index.js
+++ web/src/components/Map/index.js
@@ -1,40 +1,65 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { useState } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 import './styles.css';
 
-const Marker = ({ children }) => children;
+let devPack = [];
+function Map() {
+
+  const [selectedDev, setselectedDev] = useState(null);
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: -22.9261312, lng: -43.261952 }}
+    >
+      {devPack.map(dev => (
+        <Marker
+          key={dev._id}
+          position={{
+            lat: dev.location.coordinates[1],
+            lng: dev.location.coordinates[0]
+          }}
+          icon={{
+            url: `${dev.avatar_url}`,
+            scaledSize: new window.google.maps.Size(25, 25)
+          }}
+          onClick={() => { setselectedDev(dev) }}
+        />
+      ))}
+      {
+        selectedDev && (
+          <InfoWindow
+            position={{
+              lat: selectedDev.location.coordinates[1],
+              lng: selectedDev.location.coordinates[0]
+            }}
+            onCloseClick={() => setselectedDev(null)}
+          >
+            <div className="div-info">
+              <img src={selectedDev.avatar_url} />
+              <h3>{selectedDev.name}</h3>
+            </div>
+          </InfoWindow>
+        )
+      }
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function MyMap({ devs }) {
+  devPack = [...devs];
   return (
-    <div>
-      <div className='map-box'>
-        <div style={{ height: "70vh", width: "100%" }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.REACT_APP_API_GOOGLEMAPS }}
-            defaultCenter={{ lat: -22.9261312, lng: -43.261952 }}
-            defaultZoom={10}
-            yesIWantToUseGoogleMapApiInternals
-          >
-            {devs.map(dev => (
-              <Marker
-                key={dev._id}
-                lat={dev.location.coordinates[1]}
-                lng={dev.location.coordinates[0]}
-              >
-                <div className="map-marker">
-                  <button
-                    className="dev-marker"
-                    onClick={() => console.log(dev.name)}>
-                    <img src={dev.avatar_url} alt={dev.name} />
-                  </button>
-                </div>
-              </Marker>
-            ))}
-          </GoogleMapReact>
-        </div>
-      </div>
-    </div >
+    <div style={{ width: "100%", height: '70vh' }}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_GOOGLEMAPS}`}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
+    </div>
   );
 }
 
